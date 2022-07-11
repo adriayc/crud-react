63f1ed5034f652d29ebf7b6b2908eaba2a30e38d
Adriano Ayala <adriano.ayc@gmail.com>
2022-07-11 08:21:56 -0400

    Creating routes with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './layout/Layout'
+import HomeCustomer from './pages/HomeCustomer'
+import NewCustomer from './pages/NewCustomer'
+import EditCustomer from './pages/EditCustomer'
 
 function App() {
   return (
-    <h1>Hello World React!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/customer' element={<Layout />}>
+          <Route index element={<HomeCustomer />} />
+          <Route path="new" element={<NewCustomer />} />
+          <Route path="edit/:id" element={<EditCustomer />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
